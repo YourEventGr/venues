c6cbd27f82739eb978fafb8c15ba8ffe8f168e6d
Luís Miranda <luispaulomiranda@gmail.com>
2014-03-29 16:30:26 +0000

    fixed layout; added new attributes to venues migrations

--- db/migrate/20140329154542_create_venues.rb
+++ db/migrate/20140329154542_create_venues.rb
@@ -1,10 +1,13 @@
 class CreateVenues < ActiveRecord::Migration
   def change
     create_table :venues do |t|
-      t.string :name
-      t.text :title
-      t.integer :capacity
-
+      t.string     :name
+      t.text       :title
+      t.string     :image
+      t.string     :website
+      t.string     :phone_number
+   	  t.text       :address
+      t.integer    :capacity
       t.timestamps
     end
   end
